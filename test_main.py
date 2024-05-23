04760a036f38a7b470eee17ec2b6ccbe0a52b18d
Maxim <maksimsazonov476@gmail.com>
2024-05-23 13:57:21 -0700

    new_test

--- test_main.py
+++ test_main.py
@@ -3,24 +3,22 @@ from main import app
 
 client = TestClient(app)
 
-
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'application/json'
     assert response.json() == {"message": "World"}
 
+def test_predict_sentiment():
+    positive_sentences = ["I love machine learning!", "Machine learning is great!"]
+    negative_sentences = ["I hate machine learning!", "Machine learning is terrible!"]
 
-def test_predict_positive():
-    response = client.post("/predict/",
-                           json={"text": "I like machine learning!"})
-    json_data = response.json()
-    assert response.status_code == 200
-    assert json_data['label'] == 'POSITIVE'
+    for sentence in positive_sentences:
+        response = client.post("/predict/", json={"text": sentence})
+        assert response.status_code == 200
+        assert response.json()['label'] == 'POSITIVE'
 
-
-def test_predict_negative():
-    response = client.post("/predict/",
-                           json={"text": "I hate machine learning!"})
-    json_data = response.json()
-    assert response.status_code == 200
-    assert json_data['label'] == 'NEGATIVE'
+    for sentence in negative_sentences:
+        response = client.post("/predict/", json={"text": sentence})
+        assert response.status_code == 200
+        assert response.json()['label'] == 'NEGATIVE'
